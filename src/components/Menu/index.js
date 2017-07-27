caba8d7dbb6d0cb1701ec470e6b15d630bce4326
David Bachmann <dabbi@dabbi.is>
2017-07-27 15:50:08 +0000

    Link to case studies from menu.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,14 +3,22 @@ import './styles.css'
 
 const Menu = ({}) =>
   <nav className="Menu">
-    <a href="#" className="Menu-logo">
+    <a href="/" className="Menu-logo">
       <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/138822/aranja-logo.svg" alt="" />
     </a>
     <ul>
-      <li className="Menu-item is-active">What we do</li>
-      <li className="Menu-item">Who we are</li>
-      <li className="Menu-item">Case studies</li>
-      <li className="Menu-item">Work with us</li>
+      <li className="Menu-item">
+        <a href="/">Home</a>
+      </li>
+      <li className="Menu-item">
+        <a href="/team">The team</a>
+      </li>
+      <li className="Menu-item">
+        <a href="/casestudies">Case studies</a>
+      </li>
+      <li className="Menu-item">
+        <a href="/">Get in touch</a>
+      </li>
     </ul>
   </nav>
 
