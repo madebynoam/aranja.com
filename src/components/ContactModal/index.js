9ca4602fdba17d1deacf7e211cb6d6aa6d139aa3
Birkir Karlsson <birkir.k@me.com>
2017-07-27 14:13:16 +0000

    Contact modal setup

---
+++ src/components/ContactModal/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './styles.css'
+
+const ContactModal = ({}) =>
+  <div className="ContactModal">
+    <button className="ContactModal-closeButton">&#9587;</button>
+  </div>
+
+export default ContactModal
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactModal from '../components/ContactModal'
 import Home from './home'
 
 export default [
@@ -11,9 +12,17 @@ export default [
         api.getEntries({ content_type: 'showOff' }),
       ])
 
-      const content = pages.items.find(page => page.sys.id === '4TC4xZTIYokUiC2IecUOc6')
+      const content = pages.items.find(
+        page => page.sys.id === '4TC4xZTIYokUiC2IecUOc6'
+      )
 
       return <Home content={content} services={services} showOffs={showOffs} />
+    },
+  },
+  {
+    path: '/contact',
+    action() {
+      return <ContactModal />
     },
   },
   {
