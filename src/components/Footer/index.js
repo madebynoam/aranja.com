729b7a771886ee39f457f14911b549fcb7590cfe
David Bachmann <dabbi@dabbi.is>
2017-08-01 09:37:37 +0000

    Pimp footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import { Body1, Body2, H1, H2, H3 } from '../../typography'
-import Email from './email.svg'
-import Facebook from './facebook.svg'
-import Twitter from './twitter.svg'
+import github from './github.svg'
+import facebook from './facebook.svg'
+import twitter from './twitter.svg'
 import './styles.css'
 
 const Footer = ({}) =>
-  <section className="Footer Section">
-    <H2 className="is-inverted" bottom="small">
-      Contact us
+  <section className="Section Footer">
+    <H2 className="Footer-title" top="medium">
+      Get in touch
     </H2>
-    <div className="Footer-contactBox">
-      <a
-        href="mailto:aranja@aranja.com"
-        className="Footer-contactBlock"
-        style={{ backgroundImage: `url(${Email}` }}
-      />
-      <a
-        href="https://www.facebook.com/aranja.is/"
-        target="_blank"
-        className="Footer-contactBlock"
-        style={{ backgroundImage: `url(${Facebook}` }}
-      />
-      <a
-        href="https://twitter.com/aranjastudio"
-        target="_blank"
-        className="Footer-contactBlock"
-        style={{ backgroundImage: `url(${Twitter}` }}
-      />
+    <div className="Footer-column">
+      <h1 className="Footer-legend">Email us</h1>
+      <Body2>team@aranja.com</Body2>
+    </div>
+    <div className="Footer-column">
+      <h1 className="Footer-legend">Talk to us</h1>
+      <Body2>(+354) 7878765</Body2>
+    </div>
+    <div className="Footer-column">
+      <h1 className="Footer-legend">Follow us</h1>
+      <div className="Footer-social">
+        <a href="/" className="Footer-socialIcon">
+          <img src={twitter} alt="" />
+        </a>
+        <a href="/" className="Footer-socialIcon">
+          <img src={facebook} alt="" />
+        </a>
+        <a href="/" className="Footer-socialIcon">
+          <img src={github} alt="" />
+        </a>
+      </div>
     </div>
   </section>
 
