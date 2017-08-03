c347cd0f1e64c83141f0b020224d2546385a3212
David Bachmann <dabbi@dabbi.is>
2017-08-03 13:24:13 +0000

    Turn Hero into stateful component. Prepare for Hero slide functionality and initial animation.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,21 +1,56 @@
 import React from 'react'
-import { H1, H2, H3, Body1, Body2, Chapter } from '../../typography'
+import classNames from 'classnames'
+import { H1, Chapter } from '../../typography'
 import img01 from './Aranja00067.jpg'
+import img02 from './Aranja00663.jpg'
 import './styles.css'
 import logo from './logo.svg'
 import Menu from '../Menu'
 import ScrollIndicator from '../ScrollIndicator'
 
-const Hero = ({}) =>
-  <div className="Hero">
-    <div className="Hero-content" style={{ backgroundImage: `url(${img01})` }}>
-      <img className="Hero-logo" src={logo} alt="" />
-      <div className="Hero-text">
-        <H1>Team work is the name of the game</H1>
-        <Chapter>Read case study</Chapter>
+const items = [
+  {
+    background: img01,
+    title: 'Team work is the name of the game',
+    link: '/',
+  },
+  {
+    background: img02,
+    title: 'We\'re looking for a talented Front-end Engineer',
+    link: '/',
+  },
+]
+
+class Hero extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      activeSlide: 0,
+    }
+  }
+  render() {
+    const { activeSlide } = this.state
+
+    return (
+      <div className="Hero">
+        {items.map((item, index) =>
+          <div
+            className={classNames('Hero-item', index === activeSlide && 'is-active')}
+            style={{ backgroundImage: `url(${item.background})` }}
+          >
+            <img className="Hero-logo" src={logo} alt="" />
+            <div className="Hero-text">
+              <H1>
+                {item.title}
+              </H1>
+              <Chapter>Read case study</Chapter>
+            </div>
+          </div>,
+        )}
+        <ScrollIndicator />
       </div>
-    </div>
-    <ScrollIndicator />
-  </div>
+    )
+  }
+}
 
 export default Hero
