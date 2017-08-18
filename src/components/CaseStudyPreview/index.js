b250a74378962fdb1fdb50e03a9b0563b799f4f3
David Bachmann <dabbi@dabbi.is>
2017-08-18 13:51:51 +0000

    Add parallaxing decorative letters behind case study previews

--- src/components/CaseStudyPreview/index.js
+++ src/components/CaseStudyPreview/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import { H1, Body2 } from '../../typography'
 import Section from '../Section'
+import Parallax from '../Parallax'
 import './styles.css'
 
 class CaseStudyPreview extends Component {
@@ -9,22 +10,27 @@ class CaseStudyPreview extends Component {
     const { title, description, year, img, alignment, url } = this.props
     return (
       <Section top="small" bottom="small">
-        <a href={url} className={classNames('CaseStudyPreview', alignment === 'right' && 'is-aligned-right')}>
-          <div className="CaseStudyPreview-content">
-            <div className="CaseStudyPreview-title">
-            <H1>
+        <Parallax clamp>
+          <a href={url} className={classNames('CaseStudyPreview', alignment === 'right' && 'is-aligned-right')}>
+            <div className="CaseStudyPreview-content">
+              <div className="CaseStudyPreview-title">
+              <H1>
+                {title}
+              </H1>
+              </div>
+              <div className="CaseStudyPreview-description">
+                <Body2>{description}</Body2>
+              </div>
+              <div className="CaseStudyPreview-link">
+                <a href="/kolibri">View case study</a>
+              </div>
+            </div>
+            <div className="CaseStudyPreview-background" style={{ backgroundImage: `url(${img})` }}></div>
+            <div className="CaseStudyPreview-parallaxTitle">
               {title}
-            </H1>
             </div>
-            <div className="CaseStudyPreview-description">
-              <Body2>{description}</Body2>
-            </div>
-            <div className="CaseStudyPreview-link">
-              <a href="/kolibri">View case study</a>
-            </div>
-          </div>
-          <div className="CaseStudyPreview-background" style={{ backgroundImage: `url(${img})` }}></div>
-        </a>
+          </a>
+        </Parallax>
       </Section>
     )
   }
