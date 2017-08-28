b109140d3eaef4a702e5130d62b79fd04cb6af2e
David Bachmann <dabbi@dabbi.is>
2017-08-28 16:39:54 +0000

    Blue color overlay only on Heroes with 'cover'. Left align CaseStudyGrid images.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -9,7 +9,7 @@ class Hero extends Component {
   render() {
     const { hero, title, darken, cover } = this.props
     return (
-      <div className={classNames('Hero', darken && 'darken')}>
+      <div className={classNames('Hero', darken && 'darken', cover && 'cover')}>
         <div className="Hero-content">
           <H1 className="Hero-heading">{title}</H1>
         </div>
