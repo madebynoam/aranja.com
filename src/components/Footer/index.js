49be2a75df6dbe0451994511fb0c1ac4a0687e44
David Bachmann <dabbi@dabbi.is>
2017-08-29 13:35:19 +0000

    Prepare for Footer magic

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,69 @@
-import React from 'react'
-import { Body1, Body2, H1, H2, H3 } from '../../typography'
+import React, { Component } from 'react'
+import { Body2, H2 } from '../../typography'
 import github from './github.svg'
 import facebook from './facebook.svg'
 import twitter from './twitter.svg'
 import './styles.css'
 
-const Footer = ({}) =>
-  <section className="Footer">
-    <H2 className="Footer-title" top="medium">
-      Get in touch
-    </H2>
-    <div className="Footer-column">
-      <h1 className="Footer-legend">Email us</h1>
-      <Body2><a href="mailto:hello@aranja.com">hello@aranja.com</a></Body2>
-    </div>
-    <div className="Footer-column">
-      <h1 className="Footer-legend">Talk to us</h1>
-      <Body2><a href="tel:+3546968666">(+354) 696-8666</a></Body2>
-    </div>
-    <div className="Footer-column">
-      <h1 className="Footer-legend">Follow us</h1>
-      <div className="Footer-social">
-        <a href="https://twitter.com/aranjastudio" className="Footer-socialIcon">
-          <img src={twitter} alt="Aranja on Twitter" />
-        </a>
-        <a href="https://facebook.com/aranja.is" className="Footer-socialIcon">
-          <img src={facebook} alt="Aranja on Facebook" />
-        </a>
-        <a href="https://github.com/aranja" className="Footer-socialIcon">
-          <img src={github} alt="Aranja on Github" />
-        </a>
-      </div>
-    </div>
-  </section>
+const data = [
+  {
+    title: 'Email us',
+    content: 'hello@aranja.com',
+    link: 'mailto:hello@aranja.com'
+  },
+  { title: 'Talk to us', content: '(+354) 696-8666', link: 'tel:+3546968666' }
+]
 
+class Footer extends Component {
+  // Todo: https://github.com/aranja/new-aranja/issues/27
+
+  render() {
+    return (
+      <section
+        className="Footer"
+        ref={ref => {
+          this.ref = ref
+        }}
+      >
+        <H2 className="Footer-title" top="medium">
+          Get in touch
+        </H2>
+
+        {data.map((data, index) =>
+          <div className="Footer-column">
+            <h1 className="Footer-legend">
+              {data.title}
+            </h1>
+            <Body2>
+              <a href={data.link}>
+                {data.content}
+              </a>
+            </Body2>
+          </div>
+        )}
+
+        <div className="Footer-column">
+          <h1 className="Footer-legend">Follow us</h1>
+          <div className="Footer-social">
+            <a
+              href="https://twitter.com/aranjastudio"
+              className="Footer-socialIcon"
+            >
+              <img src={twitter} alt="Aranja on Twitter" />
+            </a>
+            <a
+              href="https://facebook.com/aranja.is"
+              className="Footer-socialIcon"
+            >
+              <img src={facebook} alt="Aranja on Facebook" />
+            </a>
+            <a href="https://github.com/aranja" className="Footer-socialIcon">
+              <img src={github} alt="Aranja on Github" />
+            </a>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
 export default Footer
--- src/components/Parallax/index.js
+++ src/components/Parallax/index.js
@@ -8,13 +8,13 @@ class Parallax extends Component {
   }
 
   translate() {
-    const { clamp } = this.props
+    const { clamp, variableName = 'parallax' } = this.props
     const { viewHeight, scrollPosition } = service.data
     const viewBottom = scrollPosition + viewHeight
     const ratio = (this.top - viewBottom) / (scrollPosition - viewBottom)
     const appliedRatio = clamp ? Math.min(1, Math.max(0, ratio)) : ratio
 
-    this.element_.style.setProperty('--parallax', appliedRatio)
+    this.element_.style.setProperty(`--${variableName}`, appliedRatio)
   }
 
   cache() {
