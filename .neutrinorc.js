ec5730ce33a0f78a9880ea6a3aaf0b2d706ea0f6
Eirikur Nilsson <eirikur@nilsson.is>
2017-08-31 00:27:39 +0000

    Upgrade to latest tux

---
+++ .neutrinorc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  use: [
+    [
+      'neutrino-preset-tux',
+      {
+        html: {
+          document: 'src/Html',
+        },
+      },
+    ],
+    neutrino => neutrino.config.entry('vendor').add('react').add('react-dom'),
+    // Fix svg imports: https://github.com/mozilla-neutrino/neutrino-dev/issues/272
+    neutrino => {
+      neutrino.config.module
+        .rule('svg')
+        .use('url')
+        .loader(require.resolve('file-loader'))
+        .options({ limit: 8192 })
+    },
+  ],
+}
