bbe42aa2d136beee7664aeae25e0a1a7bd626a31
David Bachmann <dabbi@dabbi.is>
2017-09-01 11:26:44 +0000

    Added ScreenshotsGalore that animate in

--- src/components/ContentSection/ScreenshotsGalore/index.js
+++ src/components/ContentSection/ScreenshotsGalore/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import classNames from 'classnames'
+import Parallax from '../../Parallax'
+import { withReveal } from '../../../hoc/withReveal'
+import './styles.css'
 
-const ScreenshotsGalore = ({ screenshots }) => (
-  <div className="ScreenshotsGalore">
-    {screenshots.map(shot => 
-    <img src={shot} alt="" />
-    )}
+const Wrapped = ({ img, isVisible }) => (
+  <div className={classNames('ScreenshotsGalore-wrap', isVisible && 'is-visible')}>
+    <div
+      className="ScreenshotsGalore-image"
+      style={{ backgroundImage: `url(${img})` }}
+    />
   </div>
 )
+
+const ScreenshotsGalore = ({ screenshots }) => {
+  const Wrapper = withReveal(Wrapped)
+  return (
+  <Parallax>
+    <div className="ScreenshotsGalore">
+      {screenshots.map(shot => (
+        <Wrapper img={shot.fields.file.url} key={shot.fields.title} />
+      ))}
+    </div>
+  </Parallax>
+  )
+}
 
 export default ScreenshotsGalore
