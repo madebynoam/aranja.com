dd11dc6b6759af1020d3ce8f79942dca706f15c5
David Bachmann <dabbi@dabbi.is>
2017-09-03 19:11:27 +0000

    Add animated text to hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import { H1, H3 } from '../../typography'
 import ScrollIndicator from '../ScrollIndicator'
+import AnimatedText from '../AnimatedText'
 import HeroPicture from '../Picture/HeroPicture'
+
 import './styles.css'
 
 class Hero extends Component {
   render() {
     const { hero, title, darken, cover } = this.props
-    return (
-      <div className={classNames('Hero', darken && 'darken', cover && 'cover')}>
+    return <div className={classNames("Hero", "darken", darken && "darken", cover && "cover")}>
         <div className="Hero-content">
-          <H1 className="Hero-heading">{title}</H1>
+          <h1 className="Hero-text">
+            <AnimatedText text={['At Aranja, we like to ', 'do things properly' ]}/>
+          </h1>
         </div>
         <HeroPicture src={hero.hero.src} cover={cover} />
         <ScrollIndicator />
-      </div>
-    )
+      </div>;
   }
 }
 
