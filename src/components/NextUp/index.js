505d55ce3c9f575a7cde60134be1662f521966d7
David Bachmann <dabbi@dabbi.is>
2017-09-05 21:08:40 +0000

    Commit out NextUp section for now.

--- src/components/NextUp/index.js
+++ src/components/NextUp/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Body1, Body2, H1, H2, H3, H4 } from '../../typography'
-import ArrowButton from '../ArrowButton'
+// import { Body1, Body2, H1, H2, H3, H4 } from '../../typography'
+// import ArrowButton from '../ArrowButton'
 import './styles.css'
 
-const NextUp = () =>
-  <div className="NextUp">
-    {/* <H3>Next page</H3>
-    <ArrowButton>Our work</ArrowButton> */}
+const NextUp = ({ next }) => (
+  <div style={{ backgroundImage: `url(${next.fields.heroImage.fields.file.url})` }} className="NextUp">
   </div>
+)
 
-NextUp.propTypes = {
-}
+
 
 export default NextUp
