6239d33b23e79cfe0b35fb282165431bd80eae54
David Bachmann <dabbi@dabbi.is>
2017-09-07 11:13:55 +0000

    Style TechStack component. Fix ArrowButton hover.

--- src/components/ContentSection/TechStack/index.js
+++ src/components/ContentSection/TechStack/index.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import classNames from 'classnames'
-import { H1 } from '../../../typography'
 import './styles.css'
+import { withReveal } from '../../../hoc/withReveal'
 
-const TechStack = ({ techStack }) => {
-  console.log(techStack)
+const TechStack = ({ techStack, isVisible }) => {
   return (
-    <div className="TechStack">
+    <div className={classNames('TechStack', isVisible && 'is-visible')}>
       {techStack &&
         Array.isArray(techStack) &&
         techStack.map(item => (
           <div className="TechStack-item">
-            <div className="TechStack-name">{item.fields.techName}</div>
-            <div className="TechStack-description">
-              {item.fields.techDescription}
-            </div>
             <img
               className="TechStack-logo"
               src={item.fields.icon.fields.file.url}
             />
+            <div className="TechStack-info">
+              <div className="TechStack-name">{item.fields.techName}</div>
+              <div className="TechStack-description">
+                {item.fields.techDescription}
+              </div>
+            </div>
           </div>
         ))}
     </div>
   )
 }
 
-export default TechStack
+export default withReveal(TechStack)
