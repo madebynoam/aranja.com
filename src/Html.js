222ae5c67f8f0d382a929ae6b74fcb2b82b63145
David Bachmann <dabbi@dabbi.is>
2017-09-08 15:57:41 +0000

    Add metadata and ganalytics

--- src/Html.js
+++ src/Html.js
@@ -33,10 +33,28 @@ export default class extends Document {
             helmet.link.toComponent()
           ]}
           <TypeKit kitId="kee6nkk" />
+          <meta property="og:url" content="http://aranja.com/" />
+          <meta property="og:title" content="Aranja" />
+          <meta
+            property="og:description"
+            content="We build interactive websites."
+          />
+          <link rel="shortcut icon" href="favicon.ico" />
         </Head>
         <Body>
           <App />
           <Footer />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-49724456-1', 'aranja.com');
+          ga('send', 'pageview');`
+            }}
+          />
         </Body>
       </Html>
     )
