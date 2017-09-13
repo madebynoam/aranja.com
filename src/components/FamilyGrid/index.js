6a3fd27efc730926253f3321349f4eeda57ce2f4
David Bachmann <dabbi@dabbi.is>
2017-09-13 09:51:39 +0000

    Grid layout and gap

--- src/components/FamilyGrid/index.js
+++ src/components/FamilyGrid/index.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import Parallax from '../Parallax'
 import family1 from './family1.jpg'
 import family2 from './family2.jpg'
 import family3 from './family3.jpg'
-import family4 from './family4.jpg'
 import { withReveal } from '../../hoc/withReveal'
 import './styles.scss'
 
@@ -25,12 +24,6 @@ const FamilyGrid = ({ isVisible }) => (
           <div
             className="Family-image"
             style={{ backgroundImage: `url(${family1})` }}
-          />
-        </div>
-        <div className="Family-item">
-          <div
-            className="Family-image"
-            style={{ backgroundImage: `url(${family4})` }}
           />
         </div>
         <div className="Family-item">
