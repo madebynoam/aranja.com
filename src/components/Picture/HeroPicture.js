ed1b385dc51c34a6e0286b34e24afd065aa15a38
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2018-03-04 21:30:18 +0000

    Load thumbnail before large image

--- src/components/Picture/HeroPicture.js
+++ src/components/Picture/HeroPicture.js
@@ -6,51 +6,54 @@ import Picture from './Picture'
 import { withReveal } from '../../hoc/withReveal'
 
 class HeroPicture extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isMounted: false,
-      isLoaded: false,
-    }
+  state = {
+    isReady: false,
+    isLoaded: false,
   }
 
   onLoad = () => {
     this.setState({ isLoaded: true })
   }
 
+  onReady = () => {
+    this.setState({ isReady: true })
+  }
+
   render() {
-    const { src, cover, parallax, isVisible, ...props } = this.props
-    const { isLoaded } = this.state
+    const { src, cover, isVisible, ...props } = this.props
+    const { isLoaded, isReady } = this.state
 
     if (!Array.isArray(src)) {
       console.error('HeroPicture needs width/height metadata.')
       return <Picture src={src} {...props} />
     }
-
+    
     const smallest = minBy(src, 'width')
     const main = src[0]
     const aspectRatio = main.width / main.height
     const style = !cover ? { paddingTop: `${100 / aspectRatio}%` } : null
 
     return (
-      <picture
-        className={classNames('HeroPicture', isVisible && 'is-visible', isLoaded && 'is-loaded', parallax && 'withParallax')}
+      <div
+        className={classNames('HeroPicture', isVisible && 'is-visible', isLoaded && 'is-loaded')}
         style={style}
       >
-        <Img
-          className="HeroPicture-image"
-          src={src}
-          role="presentation"
-          onLoad={this.onLoad}
-          {...props}
-        />
         <img
           className="HeroPicture-thumbnail"
           src={smallest.src}
-          role="presentation"
-          {...props}
+          onLoad={this.onReady}
         />
-      </picture>
+        {isReady && <picture
+          style={style}>
+          <Img
+            className="HeroPicture-image"
+            src={src}
+            role="presentation"
+            onLoad={this.onLoad}
+            {...props}
+          />
+        </picture>}
+      </div>
     )
   }
 }
